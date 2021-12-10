8023b61544a1d4a3100ad2041ddc7a2a957b42f1
Matt Nicolls <matt.nicolls@lifeomic.com>
2021-12-10 12:07:43 +0100

    d10

---
+++ d10/pt1.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+SOURCE_FILE = './numbers.txt'
+
+CHAR_POINTS = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137,
+}
+
+CHARACTER_PAIRINGS = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+}
+
+
+def run():
+  data = []
+  with open(SOURCE_FILE, 'r') as f:
+    for line in f:
+      data.append(line.strip())
+
+  illegal_character_counts = defaultdict(int)
+  for line in data:
+    stack = []
+    for char in line:
+      if char in CHARACTER_PAIRINGS.keys():
+        stack.append(char)
+      else:
+        if CHARACTER_PAIRINGS[stack.pop()] != char:
+          illegal_character_counts[char] += 1
+          break
+
+  print(sum([CHAR_POINTS[char] * count for char, count in illegal_character_counts.items()]))
+
+
+if __name__ == '__main__':
+  run()
---
+++ d10/pt2.py
@@ -0,0 +1,50 @@
+from functools import reduce
+
+SOURCE_FILE = './numbers.txt'
+
+CHAR_POINTS = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}
+
+CHARACTER_PAIRINGS = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+}
+
+
+def run():
+  data = []
+  with open(SOURCE_FILE, 'r') as f:
+    for line in f:
+      data.append(line.strip())
+
+  incomplete_stacks = []
+  for line in data:
+    stack = []
+    error = False
+    for char in line:
+      if char in CHARACTER_PAIRINGS.keys():
+        stack.append(char)
+      else:
+        if CHARACTER_PAIRINGS[stack.pop()] != char:
+          error = True
+          break
+    if not error:
+      incomplete_stacks.append(stack)
+
+  line_scores = []
+  for line in incomplete_stacks:
+    remaining_chars = [CHARACTER_PAIRINGS[line.pop()] for _ in range(len(line))]
+    line_scores.append(reduce(lambda score_sum, char: (score_sum * 5) + CHAR_POINTS[char], remaining_chars, 0))
+
+  line_scores.sort()
+  print(line_scores[int((len(line_scores) - 1) / 2)])
+
+
+if __name__ == '__main__':
+  run()
